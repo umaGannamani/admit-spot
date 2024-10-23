627382e298b83c31ed60be4215fa48dcb1f72f96
uma <udayarekhachowdary96@gmail.com>
2024-10-23 13:01:35 +0530

    new

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header: React.FC = () => (
   <header>
+    <div className='heading' >
     <h1>Contact Us</h1>
     <p>Resolve your Education and Immigration Queries with our experts</p>
     <p>Reach out to us to inquire about various services offered at AdmitSpot</p>
+    </div>
   </header>
 );
 
